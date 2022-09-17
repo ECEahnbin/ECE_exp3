b104bd73aff12a2e87e0b8d87571f0a12491bdbf
ECEahnbin <ahnbin2008@uos.ac.kr>
2022-09-17 21:21:53 +0900

    Update compar_4bit.v

--- compar_4bit/compar_4bit.srcs/sources_1/new/compar_4bit.v
+++ compar_4bit/compar_4bit.srcs/sources_1/new/compar_4bit.v
@@ -2,7 +2,7 @@ module compar_4bit(a, b, x, y, z);
 input [3:0] a, b; // 4bit input
 output x, y, z; // a>b, a==b, a<b
 
-assign x = (a > b) ? 1'b1 : 1'b0;
+assign x = (a > b) ? 1'b1 : 1'b0; //
 assign y = (a == b) ? 1'b1 : 1'b0;
 assign z = (a < b) ? 1'b1 : 1'b0;
 
