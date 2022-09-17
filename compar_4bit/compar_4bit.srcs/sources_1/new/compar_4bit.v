c08031f3138a8d933477f813f93acd334e68570e
ECEahnbin <ahnbin2008@uos.ac.kr>
2022-09-17 18:51:16 +0900

    Create compar_4bit.v

---
+++ compar_4bit/compar_4bit.srcs/sources_1/new/compar_4bit.v
@@ -0,0 +1,9 @@
+module compar_4bit(a, b, x, y, z);
+input [3:0] a, b; // 4bit input
+output x, y, z; // a>b, a==b, a<b
+
+assign x = (a > b) ? 1'b1 : 1'b0;
+assign y = (a == b) ? 1'b1 : 1'b0;
+assign z = (a < b) ? 1'b1 : 1'b0;
+
+endmodule
