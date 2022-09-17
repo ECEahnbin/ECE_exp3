8fda19e7aeac0f47243d105a65a97d9d73f5c874
ECEahnbin <113012231+ECEahnbin@users.noreply.github.com>
2022-09-17 18:50:05 +0900

    Delete 4_bit_comparator/4_bit_comparator.srcs/sources_1 directory

--- 4_bit_comparator/4_bit_comparator.srcs/sources_1/new/4_bit_comparator.v
+++
@@ -1,9 +0,0 @@
-module four_bit_comparator(a, b, x, y, z);
-input [3:0] a, b; // 4bit input
-output x, y, z; // a>b, a==b, a<b
-
-assign x = (a > b) ? 1'b1 : 1'b0;
-assign y = (a == b) ? 1'b1 : 1'b0;
-assign z = (a < b) ? 1'b1 : 1'b0;
-
-endmodule
